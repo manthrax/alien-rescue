05619e2a8688081f750faa31a558a39af9ed623f
thrax <manthrax@gmail.com>
2013-01-04 15:57:06 -0800

    ports

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
-var express = require('express'),
-    sio = require('socket.io'),
-	http = require('http');
-var app = express();
-var httpServer = http.createServer(app);
-app.use(express.static(__dirname));
-var port = (process.env.PORT || 3000)
+var connect = require('connect');
+connect.createServer(
+    connect.static(__dirname)
+).listen((process.env.PORT || 3000));
 
-httpServer.listen(port);
+var fs = require('fs')
+    , http = require('http')
+    , socketio = require('socket.io');
 
-var io = sio.listen(httpServer);
-
-io.set('log level',0);
-
-io.sockets.on('message', function (msg) {
-	//console.log('Message Received: ', msg);
-	socket.broadcast.emit('message', msg);
+var chatServer = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-type': 'text/html'});
+    res.end(fs.readFileSync(__dirname + '/index.html'));
+}).listen((process.env.PORT || 3000)+1, function() {
+    console.log('Listening at: http://localhost:'+((process.env.PORT || 3000)+1));
 });
-
-io.sockets.on('connection', function (socket) {
-    //console.log('Connect Received: ');
-    socket.on('message', function (msg) {
+var sockman = socketio.listen(chatServer).on('connection', function (socket) {
+     socket.on('message', function (msg) {
         //console.log('Message Received: ', msg);
         socket.broadcast.emit('message', msg);
     });
 });
+sockman.set('log level',1);

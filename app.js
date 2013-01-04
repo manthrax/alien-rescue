2279dab94990863ac63cae554fec36b5540a25c7
thrax <manthrax@gmail.com>
2013-01-04 15:35:51 -0800

    pooooorts

--- app.js
+++ app.js
@@ -1,40 +1,26 @@
 var express = require('express'),
     sio = require('socket.io'),
 	http = require('http');
-
 var app = express();
-
 var httpServer = http.createServer(app);
-
 app.use(express.static(__dirname));
-/*
-app.get('/', function(req, res){
-  res.send('<script src="/socket.io/socket.io.js"></script>\
-<script>\
-  var socket = io.connect("http://localhost:3000");\
-  socket.on("news", function (data) {\
-    alert(data.hello);\
-  });\
-</script>');
-});
-*/
+var port = (process.env.PORT || 3000)
 
-httpServer.listen(3000);
+httpServer.listen(port);
 
 var io = sio.listen(httpServer);
 
-//io.sockets.on('connection', function (socket) {
-//  socket.emit('news', { hello: 'world' });
-//});
+io.set('log level',0);
+
 io.sockets.on('message', function (msg) {
 	//console.log('Message Received: ', msg);
 	socket.broadcast.emit('message', msg);
 });
 
 io.sockets.on('connection', function (socket) {
-    console.log('Connect Received: ');
+    //console.log('Connect Received: ');
     socket.on('message', function (msg) {
-        console.log('Message Received: ', msg);
+        //console.log('Message Received: ', msg);
         socket.broadcast.emit('message', msg);
     });
 });
